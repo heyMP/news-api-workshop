95ab8112358adcd79e00bb59b2f01a11f114a5af
heymp <mgp140@gmail.com>
2020-06-17 11:30:33 -0400

    update

--- README.md
+++ README.md
@@ -3,6 +3,14 @@
 ```
 git clone https://github.com/heyMP/byu-news-api-workshop.git
 cd byu-news-api-workshop
+cp .env.example .env
+```
+
+Go to newsapi.org and generate an access token.  Place that access token in the .env file.
+
+Now start Docker Compose.
+
+```
 docker-compose up
 ```
 
--- server/newsClient.js
+++ server/newsClient.js
@@ -3,10 +3,12 @@ const apiKey = process.env.NEWS_SERVICE_API_KEY;
 
 module.exports = async (query) => {
   const tag = query.tag || "bitcoin" 
-  const articles = await fetch(`https://newsapi.org/v2/everything?q=${tag}&from=2019-12-30&sortBy=publishedAt&apiKey=${apiKey}`)
+  console.log(`https://newsapi.org/v2/everything?q=${tag}&from=2020-06-01&sortBy=publishedAt&apiKey=${apiKey}`)
+  const articles = await fetch(`https://newsapi.org/v2/everything?q=${tag}&from=2020-06-01&sortBy=publishedAt&apiKey=${apiKey}`)
   .then(res => {
     return res.json();
   })
-  .then(res => res.articles);
+  .then(res => res.articles)
+  .catch(err => console.log(err))
   return articles
 }
